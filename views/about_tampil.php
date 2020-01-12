5c829f3a123889575adf31a0963d77cbfc475371
Rizkikhairulfahmisirait04 <59794040+Rizkikhairulfahmisirait04@users.noreply.github.com>
2020-01-12 19:35:16 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,56 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong><font size="5">SISTEM INFORMASI SURAT MASUK & SURAT KELUAR <br> KANTOR BALAI DESA TANJUNG ALAM</font></strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            <thead>
+								<tr>
+									<th>No</th>
+									<th>Nama</th>
+									<th>NIM</th>
+									<th>Kelas</th>
+								</tr>
+							</thead>
+							<tr>
+								<td>1</td>
+								<td>RIZKI KHAIRUL FAHMI SIRAIT</td>
+								<td>16220392</td>
+								<td>Source4</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>NURLELI IDAYATI</td>
+								<td>16220387</td>
+								<td>Source4</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>RIRIN SARI</td>
+								<td>16220391</td>
+								<td>Source4</td>
+							</tr>
+	
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ views/kontak_tampil.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>Kantor Balai Desa Tanjung Alam</strong></center></label>
+                            <p align="center">
+                            Alamat:  Tanjung Alam, Dusun III, Kabupaten Asahan, Sumatera Utara, 21272
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telepon:0623-33213-53213<br>
+                            Email : balaidesatanjungalam@webmail.com<br>
+                            Fax : 0623-33213<br><br>
+                            <br>
+                          </div
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
